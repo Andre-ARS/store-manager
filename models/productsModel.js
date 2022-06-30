a47c1440b724d44189372f86651b2ee89f1814c7
Andre <andre360.ars@gmail.com>
2022-06-29 21:26:19 -0300

    feat(productsModel): :sparkles: Cria função que busca um produto, especifico, a partir do id passado
    Essa função foi feita na camada models, e se comunica diretamente com o banco de dados, retornando  um objeto com o produto procurado
    
    Req1

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,11 +1,19 @@
 const { connection } = require('../helpers');
 
 const getAll = async () => {
-  const [result] = await connection.execute(
-    'SELECT * FROM StoreManager.products',
-  );
+  const query = 'SELECT * FROM StoreManager.products ORDER BY id';
+  const [result] = await connection.execute(query);
 
   return result;
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+  const query = `SELECT * FROM StoreManager.products
+    WHERE id=?`;
+
+  const [[result]] = await connection.execute(query, [id]);
+
+  return result;
+};
+
+module.exports = { getAll, getById };
