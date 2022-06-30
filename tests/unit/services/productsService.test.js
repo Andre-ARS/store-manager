85b682b453b790aa4ecb8acbc1db550b6fd82d30
Andre <andre360.ars@gmail.com>
2022-06-30 15:38:25 -0300

    test(tests): :white_check_mark: Adiciona teste para a função de criação de produtos em services

--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -4,7 +4,7 @@ const { expect } = require("chai");
 const { productsModel } = require("../../../models");
 const { productsService } = require("../../../services");
 
-describe("Lista todos produtos", () => {
+describe("Tests the function getAll in services", () => {
   before(async () => {
     const execute = [
       { id: 1, name: "Martelo de Thor" },
@@ -19,8 +19,8 @@ describe("Lista todos produtos", () => {
     productsModel.getAll.restore();
   });
 
-  describe("quando listado com sucesso", () => {
-    it("retorna um array", async () => {
+  describe("Once succedes", () => {
+    it("Returns an array", async () => {
       const response = await productsService.getAll();
 
       expect(response.result).to.be.a("array");
@@ -28,9 +28,9 @@ describe("Lista todos produtos", () => {
   });
 });
 
-describe("Busca produto pelo id", () => {
-  describe("quando o id não existe", () => {
-    it("retorna o codigo certo", async () => {
+describe("Tests the function getById in services", () => {
+  describe("If the id don't exists", () => {
+    it("returns the right status code", async () => {
       const response = await productsService.getById(50);
 
       expect(response.code).to.be.equal(404);
@@ -47,11 +47,53 @@ describe("Busca produto pelo id", () => {
     productsModel.getAll.restore();
   });
 
-  describe("quando o id existe", () => {
-    it("retorna o objeto do produto", async () => {
+  describe("If the id exists", () => {
+    it("Returns the product's object", async () => {
       const response = await productsService.getById(1);
 
       expect(response).to.be.a("object");
+    });
+  });
+});
+
+describe("Tests the function create in services", () => {
+  describe('Valids the name', () => {
+    it("can't be undefined", async () => {
+      const { code, result } = await productsService.create('');
+
+      expect(code).to.be.equal(400);
+      expect(result.message).to.be.equal('"name" is required');
+    });
+
+    it('must be at least 5 characters long', async () => {
+      const { code, result } = await productsService.create('prod');
+
+      expect(code).to.be.equal(422);
+      expect(result.message).to.be.equal(
+        '"name" length must be at least 5 characters long'
+      );
+    });
+  });
+  
+  before(async () => {
+    const product = {
+      id: 4,
+      name: "produto x",
+    };
+
+    sinon.stub(productsModel, "create").resolves(product);
+  });
+
+  after(async () => {
+    productsModel.create.restore();
+  });
+
+  describe('Once succeeds', () => {
+    it('returns the right status code', async () => {
+      const { code, result } = await productsService.create('produto x');
+
+      expect(code).to.be.equal(201);
+      expect(result).to.be.a('object');
     });
   });
 });
