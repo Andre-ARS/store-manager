5f1f99e39d4683148db7039a4435b452a2760ec0
Andre <andre360.ars@gmail.com>
2022-07-01 16:06:12 -0300

    feat(routes): :sparkles: Cria nova rota get/sales, que lista todas as vendas do banco
    ]

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -3,6 +3,8 @@ const { salesController } = require('../controllers');
 
 const salesRoute = express.Router();
 
+salesRoute.get('/', salesController.getAllSales);
+
 salesRoute.post('/', salesController.addSale);
 
 module.exports = salesRoute;
