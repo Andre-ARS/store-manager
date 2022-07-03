75b12c3d7c1cc863da319b5a2b1ea2316a37898d
Andre <andre360.ars@gmail.com>
2022-07-03 18:53:45 -0300

    test(tests): :white_check_mark: Adiciona Testes para a função getsaleById em models

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -37,20 +37,22 @@ describe("Tests the function addSale in models", () => {
 
 describe("Tests the function getAllSales in models", () => {
   before(async () => {
-    const execute = [[
-      {
-        saleId: 1,
-        date: "2022-07-03T02:21:54.000Z",
-        productId: 1,
-        quantity: 5,
-      },
-      {
-        saleId: 1,
-        date: "2022-07-03T02:21:54.000Z",
-        productId: 2,
-        quantity: 10,
-      },
-    ]];
+    const execute = [
+      [
+        {
+          saleId: 1,
+          date: "2022-07-03T02:21:54.000Z",
+          productId: 1,
+          quantity: 5,
+        },
+        {
+          saleId: 1,
+          date: "2022-07-03T02:21:54.000Z",
+          productId: 2,
+          quantity: 10,
+        },
+      ],
+    ];
 
     sinon.stub(connection, "execute").resolves(execute);
   });
@@ -65,5 +67,49 @@ describe("Tests the function getAllSales in models", () => {
 
       expect(response).to.be.a("array");
     });
+  });
+});
+
+describe("Tests the function getSaleById in models", () => {
+  let execute = [
+    [
+      {
+        date: "2022-07-03T02:21:54.000Z",
+        productId: 1,
+        quantity: 5,
+      },
+      {
+        date: "2022-07-03T02:21:54.000Z",
+        productId: 2,
+        quantity: 10,
+      },
+    ],
+  ];
+
+  sinon.stub(connection, "execute").resolves(execute);
+  
+  describe("If the id exists", () => {
+    it("Returns an array with length greater than 0", async () => {
+      const response = await salesModel.getSaleById(1);
+      
+      expect(response).to.be.a("array");
+      expect(response.length > 0).to.be.equal(true);
+    });
+    connection.execute.restore();
+  });
+
+  
+  execute = [[]];
+
+  sinon.stub(connection, "execute").resolves(execute);
+
+  describe("If the id don't exists", () => {
+    it("Returns an empty array", async () => {
+      const response = await salesModel.getSaleById(5);
+      
+      expect(response).to.be.a("array");
+      expect(response.length > 0).to.be.equal(false);
+    });
+    connection.execute.restore();
   });
 });
