1c0cbdb0a37733b5bccbede7a8b0a047fd5a0cfd
Andre <andre360.ars@gmail.com>
2022-07-03 00:47:55 -0300

    test(tests): :white_check_mark: Adiciona teste para a função getAllSales em models

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -34,3 +34,36 @@ describe("Tests the function addSale in models", () => {
     });
   });
 });
+
+describe("Tests the function getAllSales in models", () => {
+  before(async () => {
+    const execute = [[
+      {
+        saleId: 1,
+        date: "2022-07-03T02:21:54.000Z",
+        productId: 1,
+        quantity: 5,
+      },
+      {
+        saleId: 1,
+        date: "2022-07-03T02:21:54.000Z",
+        productId: 2,
+        quantity: 10,
+      },
+    ]];
+
+    sinon.stub(connection, "execute").resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  describe("Once succedes", () => {
+    it("Returns an array", async () => {
+      const response = await salesModel.getAllSales();
+
+      expect(response).to.be.a("array");
+    });
+  });
+});
