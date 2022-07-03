c0609c5277b278accdd070e327affd3b41d5404d
Andre <andre360.ars@gmail.com>
2022-07-03 20:06:12 -0300

    feat(productsService): :sparkles: cria função update em services
    cria função, com validações e mensagens de erro personalizadas, para atualizar as informações de um determinado produto, em services
    
    Req10

--- services/productsService.js
+++ services/productsService.js
@@ -9,7 +9,7 @@ const getAll = async () => {
 const getById = async (id) => {
   const product = await productsModel.getById(id);
 
-  if (!product) return { code: 404, result: { message: 'Product not found' } }; 
+  if (!product) return { code: 404, result: { message: 'Product not found' } };
 
   return { code: 200, result: product };
 };
@@ -27,4 +27,24 @@ const create = async (name) => {
   return { code: 201, result: product };
 };
 
-module.exports = { getAll, getById, create };
+const update = async (id, name) => {
+  const products = await productsModel.getAll();
+  const productIds = products.map(({ id: productId }) => productId);
+
+  if (!productIds.includes(id)) {
+    return { code: 404, result: { message: 'Product not found' } };
+  }
+  if (!name) return { code: 400, result: { message: '"name" is required' } };
+  if (name.length < 5) {
+    return {
+      code: 422,
+      result: { message: '"name" length must be at least 5 characters long' },
+    };
+  }
+
+  const product = await productsModel.update(id, name);
+
+  return { code: 200, result: product };
+};
+
+module.exports = { getAll, getById, create, update };
