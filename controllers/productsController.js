b8b47aaec98b6408fb12e3aa913ccdfd71420711
Andre <andre360.ars@gmail.com>
2022-07-04 14:17:00 -0300

    fix(productsController): :bug: Conserta tipo do parametro enviado no controller getById
    Transforma o parametro id em numero, na chamada da função da camada services

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -8,7 +8,7 @@ const getAll = async (_req, res) => {
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const { code, result } = await productsService.getById(id);
+  const { code, result } = await productsService.getById(Number(id));
 
   res.status(code).json(result);
 };
