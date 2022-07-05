c629e6792c6dc1c2288ce1125a31013361deb16d
Andre <andre360.ars@gmail.com>
2022-07-05 14:20:37 -0300

    test(tests): :white_check_mark: Adiciona testes para a função updateSale em salesModel

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -74,51 +74,53 @@ describe("Tests the function getAllSales in models", () => {
 describe("Tests the function getSaleById in models", () => {
   describe("success cases", () => {
     before(async () => {
-      let execute = [[
-        {
-          date: "2022-07-03T02:21:54.000Z",
-          productId: 1,
-          quantity: 5,
-        },
-        {
-          date: "2022-07-03T02:21:54.000Z",
-          productId: 2,
-          quantity: 10,
-        },
-      ]];
-  
+      let execute = [
+        [
+          {
+            date: "2022-07-03T02:21:54.000Z",
+            productId: 1,
+            quantity: 5,
+          },
+          {
+            date: "2022-07-03T02:21:54.000Z",
+            productId: 2,
+            quantity: 10,
+          },
+        ],
+      ];
+
       sinon.stub(connection, "execute").resolves(execute);
     });
-  
+
     after(async () => {
       connection.execute.restore();
     });
-  
+
     describe("If the id exists", () => {
       it("Returns an array with length greater than 0", async () => {
         const response = await salesModel.getSaleById(1);
-  
+
         expect(response).to.be.a("array");
         expect(response).to.have.length.above(0);
       });
     });
   });
-  
+
   describe("Fail cases", () => {
     before(async () => {
       let execute = [[]];
-  
+
       sinon.stub(connection, "execute").resolves(execute);
     });
-  
+
     after(async () => {
       connection.execute.restore();
     });
-  
+
     describe("If the id don't exists", () => {
       it("Returns an empty array", async () => {
         const response = await salesModel.getSaleById(5);
-    
+
         expect(response).to.be.a("array");
         expect(response).to.have.length.below(1);
       });
@@ -142,6 +144,37 @@ describe("Tests the function excludeSale in models", () => {
       const affectedRows = await salesModel.excludeSale(1);
 
       expect(affectedRows).to.be.equal(1);
+    });
+  });
+});
+
+describe("Tests the function updateSale in models", () => {
+  before(async () => {
+    const execute = [];
+
+    sinon.stub(connection, "execute").resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  describe("Once succeeds", () => {
+    it("Returns an object", async () => {
+      const changes = [
+        {
+          productId: 1,
+          quantity: 10,
+        },
+        {
+          productId: 2,
+          quantity: 50,
+        },
+      ];
+
+      const response = await salesModel.updateSale(1, changes);
+
+      expect(response).to.be.a("object");
     });
   });
 });
